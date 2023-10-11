e01856858700be16609092990a654a795894fe0f
simplyJuanjo <juanjodoolmo@gmail.com>
2023-10-11 12:16:25 +0200

    mvp claude nav lite convers summarizer

--- services/langchain.js
+++ services/langchain.js
@@ -11,6 +11,9 @@ const insights = require('../services/insights');
 const { Client } = require("langsmith")
 const { LangChainTracer } = require("langchain/callbacks");
 const { ChatBedrock } = require("langchain/chat_models/bedrock");
+const { ConversationChain } = require("langchain/chains");
+const { ChatPromptTemplate, HumanMessagePromptTemplate, SystemMessagePromptTemplate, MessagesPlaceholder } = require("langchain/prompts");
+const { BufferMemory } = require("langchain/memory");
 
 const OPENAI_API_KEY = config.OPENAI_API_KEY;
 const OPENAI_API_VERSION = config.OPENAI_API_VERSION;
@@ -145,28 +148,6 @@ const combine_extract_prompt = new PromptTemplate({
   `,
 });
 
-async function getActualEvents(patientId) {
-  return new Promise((resolve, reject) => {
-    Events.find({ "createdBy": patientId }, { "createdBy": false }, (err, eventsdb) => {
-      if (err) {
-        reject(err);
-      } else {
-        var listEventsdb = [];
-
-        eventsdb.forEach(function (eventdb) {
-          if(eventdb.checked){
-            let types = ["symptom", "drug", "allergy", "disease", "treatment", "gene", "other" ,"anomalies"];
-            if(types.includes(eventdb.type)){
-              listEventsdb.push(eventdb);
-            }
-          }
-        });
-
-        resolve(listEventsdb);
-      }
-    });
-  });
-}
 
 async function getMostCommonLanguage(blobs, containerName) {
   try {
@@ -213,99 +194,71 @@ async function translateText(text, deepl_code) {
   }
 }
 
-async function summarize(patientId, containerName, url, doc_id, filename, userId) {
+
+// This function will be a basic conversation with documents (context)
+// This will take some history of the conversation if any and the current documents if any
+// And will return a proper answer to the question based on the conversation and the documents 
+async function navigator_summarize(userId, question, conversation, context){
   try {
-    const message = {"docId": doc_id, "status": "creando resumen", "filename": filename}
-    pubsub.sendToUser(userId, message)
     // Create the models
-    const projectName = `${config.LANGSMITH_PROJECT} - ${patientId}`;
+    const projectName = `LITE - ${config.LANGSMITH_PROJECT} - ${userId}`;
     let { model, model32k, claude2 } = createModels(projectName);
-    // In this example, we use a `MapReduceDocumentsChain` specifically prompted to summarize a set of documents.
-    let url2 = url.replace(/\/[^\/]*$/, '/extracted_translated.txt');
-    let lang_url = url.replace(/\/[^\/]*$/, '/language.txt');
-    let text, doc_lang;
-    try {
-      // Try to download the translation
-      text = await azure_blobs.downloadBlob(containerName, url2);
-      doc_lang = await azure_blobs.downloadBlob(containerName, lang_url);
-    } catch (error) {
-      insights.error(error);
-      console.error('Error downloading the translated blob:', error);
-      // Handle the error and make a different call here
-      // For example:
-      let url3 = url.replace(/\/[^\/]*$/, '/extracted.txt');
-      text = await azure_blobs.downloadBlob(containerName, url3);
+
+    // Format and call the prompt
+    let cleanPatientInfo = "";
+    let i = 1;
+    for (const doc of context) {
+      cleanPatientInfo += "<Complete Document " + i + ">\n" + doc + "</Complete Document " + i + ">\n";
+      i++;
     }
-    const textSplitter = new RecursiveCharacterTextSplitter({ chunkSize: 100000 });
-    const docs = await textSplitter.createDocuments([text]);
 
-    chain = loadSummarizationChain(model32k, {
-      type: "map_reduce",
-      returnIntermediateSteps: true,
-      combineMapPrompt: combine_map_prompt,
-      combinePrompt: combine_prompt,
-    });
+    const systemMessagePrompt = SystemMessagePromptTemplate.fromTemplate(
+      `This is part of the medical information of the patient:
 
-    const res = await chain.call({
-      input_documents: docs,
-    });
-    
-    // Extract JSON from the string
-    let match
-    try{
-      match = res.text.match(/\{(.|\n)*\}/);
-    } catch (error) {
-      console.error('Error extracting JSON from the string:', error);
-      match = [{}];
-    }
-    
-    // Create a blob with the summary
-    const blob_response = await azure_blobs.createBlob(containerName, url.replace(/\/[^\/]*$/, '/summary_translated.txt'), match[0])
-    
-    // Parse the summary into JSON
-    let summaryJson, jsonString;
+      ${cleanPatientInfo}
 
-    if (match) {
-      jsonString = match[0];
-      try {
-        summaryJson = JSON.parse(jsonString);
-        // Rest of the code...
-      } catch (error) {
-        console.error('Failed to parse JSON:', jsonString);
-      }
+      You are a medical expert, based on this context with the condensed documents from the patient.`
+    );
+
+    const humanMessagePrompt = HumanMessagePromptTemplate.fromTemplate(
+      `Take a deep breath and work on this problem step-by-step.      
+      Please, answer the following question without making up any information:
+
+      <input>
+      {input}
+      </input>
+      
+      If you don't find the answer or you need more information, please, return that you don't know the answer and ask for more information.`
+    );
+
+    const chatPrompt = ChatPromptTemplate.fromMessages([systemMessagePrompt, new MessagesPlaceholder("history"), humanMessagePrompt]);
+    
+    let memory;
+    if (conversation === null) {
+      memory = new BufferMemory({ returnMessages: true, memoryKey: "history" });
     } else {
-      console.error('No JSON object found in the string');
-    }
-    
-    // Check if the doc_lang is available in DeepL
-    deepl_code = await translate.getDeeplCode(doc_lang);
-
-    // Loop through the JSON and translate the values
-    for (let key in summaryJson) {
-      if(Array.isArray(summaryJson[key])) {
-        // Translate each item in the array
-        for(let i = 0; i < summaryJson[key].length; i++) {
-          summaryJson[key][i] = await translateText(summaryJson[key][i], deepl_code);
-        }
-      } else {
-        // Translate the string
-        summaryJson[key] = await translateText(summaryJson[key], deepl_code);
+      memory = new BufferMemory({ returnMessages: true, memoryKey: "history" });
+      // Add the conversation history to the memory
+      for (const message of conversation) {
+        memory.addMessage(message);
       }
     }
+
+    const chain = new ConversationChain({
+      memory: memory,
+      prompt: chatPrompt,
+      llm: claude2,
+    });
     
-    // Reassemble the translated JSON into a string
-    const translatedSummary = JSON.stringify(summaryJson);
-    
-    const blob_response2 = await azure_blobs.createBlob(containerName, url.replace(/\/[^\/]*$/, '/summary.txt'), translatedSummary)
-    
-    // Alert the client that the summary is ready (change status in the message)
-    message.status = "resumen ready"
-    pubsub.sendToUser(userId, message)
+    const response = await chain.call({
+      input: question,
+    });
+
+    console.log(response);
   } catch (error) {
     console.log("Error happened: ", error)
     insights.error(error);
-    pubsub.sendToUser(userId, {"docId": doc_id, "status": "error summarize", "filename": filename, "error": error})
-  };
+  }
 }
 
 async function clean_and_extract(patientId, containerName, url, doc_id, filename, userId) {
@@ -328,24 +281,6 @@ async function clean_and_extract(patientId, containerName, url, doc_id, filename
       let url3 = url.replace(/\/[^\/]*$/, '/extracted.txt');
       text = await azure_blobs.downloadBlob(containerName, url3);
     }
-
-    // const textSplitter = new RecursiveCharacterTextSplitter({ chunkSize: 1000000 });
-    // const docs = await textSplitter.createDocuments([text]);
-
-    // chain = loadSummarizationChain(claude2, {
-    //   type: "map_reduce",
-    //   returnIntermediateSteps: true,
-    //   combineMapPrompt: combine_clean_prompt,
-    //   combinePrompt: combine_extract_prompt,
-    // });
-    // const chain = new LLMChain({
-    //   llm: claude2,
-    //   prompt: combine_extract_prompt,
-    // });
-
-    // const res = await chain.call({
-    //   text: text,
-    // });
 
     const res = await claude2.callPrompt(
       await combine_extract_prompt.formatPromptValue({
@@ -479,219 +414,8 @@ async function anonymize(patientId, containerName, url, doc_id, filename, userId
   });
 }
 
-
-async function summarizePatient(patientId, userId) {
-  return new Promise(async (resolve, reject) => {
-  try {
-    const message = {"status": "patient card started", "step": "summary"}
-    pubsub.sendToUser(userId, message)
-
-    // Create the models
-    const projectName = `${config.LANGSMITH_PROJECT} - ${patientId}`;
-    let { model, model32k} = createModels(projectName);
-
-    // Decrypt the patientId
-    const patientIdEncrypted = crypt.encrypt(patientId);
-    const containerName = patientIdEncrypted.substr(1)
-    // Get all the summaries for this patient
-    // List all blobs from the patient folder
-    const blobs = await azure_blobs.listContainerFiles(containerName);
-    // Filter the summaries
-    const summary_translated_blobs = blobs.filter(blob => blob.endsWith("summary_translated.txt"));
-    // Download the summaries
-    const summaries = await Promise.all(summary_translated_blobs.map(blob => azure_blobs.downloadBlob(containerName, blob)));
-
-    // Create a langchain prompt with all the summaries to generate a summary
-    let summarize_summaries_prompt = new PromptTemplate({
-      inputVariables: ["summaries"],
-      template: `Please summarize all the summaries for this patient.
-      Include the date, type of event, and any relevant details. This is the list of summaries:
-      ${"```"}{summaries}${"```"}
-
-      ---
-      SUMMARY:
-      `,
-    });
-
-    const chainSummaries = new LLMChain({
-      llm: model,
-      prompt: summarize_summaries_prompt,          
-    });
-
-    const summarizedSummaries = await chainSummaries.call({
-      summaries: summaries.join("\n\n"),
-    });
-
-    // Get all the verified events for this patient
-    // Controler de events
-    const events = await getActualEvents(patientId);
-    
-    // Filter the verified events
-    const verifiedEvents = events.filter(event => event.checked === true);
-
-    // Count the amount of each type of verified event
-    const eventCounts = verifiedEvents.reduce((counts, event) => {
-      const eventType = event.type;
-      counts[eventType] = (counts[eventType] || 0) + 1;
-      return counts;
-    }, {});
-
-    // Generate the event summary
-    let event_summary = "";
-    for (const eventType in eventCounts) {
-      const count = eventCounts[eventType];
-      event_summary += `${count} ${eventType}, :\n`;
-      // add a list of the events of this type (name, date)
-      const eventsOfType = verifiedEvents.filter(event => event.type === eventType);
-      for (const event of eventsOfType) {
-        const formattedDate = event.date ? new Date(event.date).toDateString() : "unknown";
-        event_summary += `  ${event.name} (${formattedDate})\n`;
-      }
-    }
-
-    // Create a langchain prompt with all the verified events to generate a summary
-    let summarize_events_prompt = new PromptTemplate({
-      inputVariables: ["event_summary"],
-      template: `Please summarize all the verified events for this patient. 
-      Include the date, type of event, and any relevant details. This is the list of verified events:
-      ${"```"}{event_summary}${"```"}
-
-      ---
-      SUMMARY:
-      `,
-    });
-
-    const chainEvents = new LLMChain({
-      llm: model,
-      prompt: summarize_events_prompt,          
-    });
-
-    const summarizedEvents = await chainEvents.call({
-      event_summary: event_summary,
-    });
-
-    // Create a langchain prompt with the summaries of the summaries and the event summary to generate a summary
-    let final_card_summary_prompt = new PromptTemplate({
-      inputVariables: ["summarizedSummaries", "summarizedEvents"],
-      template: `Please summarize the patient document summaries and the verified events for this patient.
-      I'm providing the summaries and the verified events for you to use as a reference:
-      Here are the doc summaries summarized:
-      ${"```"}{summarizedSummaries}${"```"}
-      Here are the verified events summarized:
-      ${"```"}{summarizedEvents}${"```"}
-    
-      I want you to create a card of presentation of this patient for the doctor.
-      Therefore take all the information you have and create a summary of the patient in a card format.
-      With this card the doctor should be able to understand the patient's situation and history.
-      The card should be in the following format:
-      ---
-      CARD SUMMARY for the PATIENT:
-      Don't output anything more than the following JSON. Even if you don't have all the information, you should output ONLY the JSON with all the keys but the values can be empty if you don't have the information.
-      Please provide your response in the form of a JSON object with the following keys (if present):
-      - 'Name'
-      - 'Age', string
-      - 'Gender'
-      - 'CurrentStatus'
-      - 'Diagnoses', which should be an array if present.
-      - 'Medication', which should be an array if present.
-      - 'Treatments', which should be an array if present.
-      - 'LaboratoryFindings', which should be an array if present.
-      - 'AdditionalInformation', which should be an array if present.
-      `,
-    });
-    
-
-    const chainFinalCardSummary = new LLMChain({
-      llm: model,
-      prompt: final_card_summary_prompt,
-    });
-
-    const finalCardSummary = await chainFinalCardSummary.call({
-      summarizedSummaries: summarizedSummaries.text,
-      summarizedEvents: summarizedEvents.text,
-    });
-
-    // console.log(finalCardSummary.text);
-
-    // Get the most common language of the original documents (download from blob)
-    const mostCommonLanguage = await getMostCommonLanguage(blobs, containerName);
-    
-    // Extract JSON from the string
-    let match = finalCardSummary.text.match(/\{(.|\n)*\}/);
-    // Create a blob with the final card summary
-    const blob_response = await azure_blobs.createBlob(containerName, 'raitofile/summary/final_card_translated.txt', match[0]);
-
-    // Parse the summary into JSON
-    let summaryJson, jsonString;
-
-    if (match) {
-      jsonString = match[0];
-      try {
-        summaryJson = JSON.parse(jsonString);
-        // Rest of the code...
-      } catch (error) {
-        console.error('Failed to parse JSON:', jsonString);
-      }
-    } else {
-      console.error('No JSON object found in the string');
-    }
-    
-    // Check if the doc_lang is available in DeepL
-    deepl_code = await translate.getDeeplCode(mostCommonLanguage);
-    
-    // Function to translate text
-    async function translateText(text, deepl_code) {
-      // Don't try to translate if text is empty or an empty array
-      if (!text || (Array.isArray(text) && text.length === 0)) {
-        return text;
-      }
-      if (deepl_code == null) {
-        // Do an Inverse Translation
-        const info = [{ "Text": text }];
-        const inverseTranslatedText = await translate.getTranslationDictionaryInvertMicrosoft2(info, mostCommonLanguage);
-        return inverseTranslatedText[0].translations[0].text;
-      } else {
-        // Do a Translation
-        return await translate.deepLtranslate(text, deepl_code);
-      }
-    }
-
-    // Loop through the JSON and translate the values
-    for (let key in summaryJson) {
-      if(Array.isArray(summaryJson[key])) {
-        // Translate each item in the array
-        for(let i = 0; i < summaryJson[key].length; i++) {
-          summaryJson[key][i] = await translateText(summaryJson[key][i], deepl_code);
-        }
-      } else {
-        // Translate the string
-        summaryJson[key] = await translateText(summaryJson[key], deepl_code);
-      }
-    }
-    
-    // Reassemble the translated JSON into a string
-    const translatedSummary = JSON.stringify(summaryJson);
-
-    const blob_response2 = await azure_blobs.createBlob(containerName, 'raitofile/summary/final_card.txt', translatedSummary)
-    // Alert the client that the final card is ready (change status in the message)
-    message.status = "patient card ready"
-    pubsub.sendToUser(userId, message)
-    resolve(true);
-  } catch (error) {
-    insights.error(error);
-    console.error(error);
-    const message = {"status": "patient card fail", "step": "summary"}
-    pubsub.sendToUser(userId, message)
-    reject(error);
-  }
-});
-}
-
-
-
 module.exports = {
-  summarize,
+  navigator_summarize,
   clean_and_extract,
   anonymize,
-  summarizePatient
 };
